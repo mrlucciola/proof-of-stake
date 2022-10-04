b00a92df6880e0731c1eabcc926a5f91e78c551e
mrlucciola <mrlucciola@pm.me>
2022-10-04 17:05:45 -0400

    create txn struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Txn<'a> {
+    sender_pubkey: &'a String,
+    recv_pubkey: &'a String,
+    amt: u128,
+    txn_type: TxnType,
+}
+enum TxnType {
+    Transfer,
+}
+
+impl<'a> Txn<'a> {
+    fn new(
+        &self,
+        sender_pubkey: &'a String,
+        recv_pubkey: &'a String,
+        amt: u128,
+        txn_type: TxnType,
+    ) -> Self {
+        Self {
+            sender_pubkey,
+            recv_pubkey,
+            amt,
+            txn_type,
+        }
+    }
+}
