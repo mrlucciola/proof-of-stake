15c9d33d9a8405bfb2560acb810b632fc1476233
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-01-03 01:01:58 -0500

    feat: #54 add test for `blockchain.add_block()`
    add `Blockchain.block()` getter

--- tests/blockchain.rs
+++ tests/blockchain.rs
@@ -4,21 +4,20 @@ use posbc::ledger::blockchain::Blockchain;
 
 pub mod accounts;
 pub mod common;
-use common::{init_users, UsersInfo};
+use common::{fxns::create_block, init_users, UsersInfo};
 
 // tests:
 
+fn init_blockchain() -> (UsersInfo, Blockchain) {
+    let users = init_users();
+    let blockchain = Blockchain::new();
+
+    (users, blockchain)
+}
+
 #[test]
 fn create_blockchain_pass() {
-    let UsersInfo {
-        main,
-        send: _,
-        recv: _,
-        test1: _,
-        test2: _,
-        test3: _,
-    } = init_users();
-    let blockchain = Blockchain::new();
+    let (users, blockchain) = init_blockchain();
 
     let blocks = blockchain.blocks();
 
@@ -28,9 +27,9 @@ fn create_blockchain_pass() {
     // all blocks must be valid (no `None` fields, correct hash, signed)
     let mut genesis = blocks.values().to_owned().next().unwrap().to_owned();
 
-    genesis.sign(&main.wallet);
+    genesis.sign(&users.main.wallet);
 
-    let is_valid = genesis.is_valid(&main.wallet).unwrap();
+    let is_valid = genesis.is_valid(&users.main.wallet).unwrap();
     assert!(is_valid, "Invalid genesis block: {:?}", is_valid);
 
     // block must be genesis
@@ -38,5 +37,34 @@ fn create_blockchain_pass() {
         Blockchain::is_genesis_block(&genesis),
         "Not genesis block: {:?}",
         genesis.id()
+    );
+}
+
+/// add a block to the blockchain
+/// Check if the block map length increased by 1
+/// Check if that new block exists
+#[test]
+fn add_block_to_blockchain_pass() {
+    let (users, mut blockchain) = init_blockchain();
+
+    let blocks_len_pre = blockchain.blocks().len();
+
+    // add a block to the chain
+    let new_block_to_add = create_block(users.main);
+    let key = new_block_to_add.id_key();
+    blockchain.add_block(new_block_to_add);
+    let blocks_len_post = blockchain.blocks().len();
+
+    // check
+    assert_eq!(
+        blocks_len_post,
+        blocks_len_pre + 1,
+        "Block count is incorrect"
+    );
+
+    // see if its the same block
+    assert!(
+        blockchain.block(&key).is_some(),
+        "Block is not in blockchain"
     );
 }
