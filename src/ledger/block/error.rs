209d4072f3e90d7b284228229637b4e582f175ff
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-04-12 19:39:46 -0400

    chore: #94 move block errors to separate file

---
+++ src/ledger/block/error.rs
@@ -0,0 +1,13 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum BlockError {
+    #[error("BlockError::EmptySignature- Invalid block: No signature")]
+    EmptySignature,
+    #[error("BlockError::EmptyId- Invalid block: No ID")]
+    EmptyId,
+    #[error("BlockError::IncorrectId- Incorrect ID")]
+    IncorrectId,
+    #[error("BlockError::InvalidSignature- {0}. Testing signature:\n{1}")]
+    InvalidSignature(ed25519_dalek::SignatureError, ed25519::Signature),
+}
