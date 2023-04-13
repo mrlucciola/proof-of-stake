89ae650d791f283c0394d1d3d4bbaaf04fbcb7e5
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-04-13 17:01:05 -0400

    chore: #101 node- more refactoring

--- src/node/mod.rs
+++ src/node/mod.rs
@@ -1,15 +1,14 @@
 mod error;
 mod getters;
 mod p2p;
-pub mod types;
+mod setters;
+mod types;
 mod utils;
-// external
-use std::net::IpAddr;
 // local
 use crate::ledger::{blockchain::Blockchain, txn_pool::TxnPool, wallet::Wallet};
-pub use {error::NodeError, p2p::P2P, types::*};
+pub use {error::NodeError, p2p::P2P, types::Result};
 
-/// ## An instance of a `Node`.
+/// ### An instance of a `Node`.
 /// A node has a wallet, an instance of blockchain, and a transaction pool.
 /// Node will sync its blockchain ledger and transaction pool with its peers
 /// via the peer-to-peer network (p2p).
@@ -28,71 +27,54 @@ pub struct Node {
 }
 
 impl Node {
-    /// ## Create new instance of Node.
+    /// ### Create a new `Node` instance.
     ///
-    /// Internal values will be updated separately, because:
-    /// 1. There may be multiple ways to initialize a value (i.e. specific to a situation);
-    /// 1. Values may be updated asynchronously;
+    /// blockchain and txn pool values will be updated separately, because:
+    /// 1. There may be multiple ways to initialize them (i.e. specific to a situation);
+    /// 1. They may be updated asynchronously;
     /// 1. It may not be necessary to initialize immediately.
-    pub fn new() -> Self {
-        Self {
+    pub fn new(p2p: P2P, wallet: Wallet) -> Self {
+        let mut new_node = Self {
             wallet: None,
             blockchain: None,
             txn_pool: None,
             p2p: None,
-        }
+        };
+
+        new_node.set_p2p(p2p).unwrap();
+        new_node.set_wallet(wallet).unwrap();
+
+        new_node
     }
 
-    /////////////////////////////////////////////////////////////////////
-    ////////////////////////////// SETTERS //////////////////////////////
-    /// ## Set the wallet for the node.
-    ///
-    /// Using a dedicated setter to standardize how the wallet gets updated.
-    ///
-    /// There will be more methods for initializing a wallet in the future.
+    /////////////////////////////////////////////////////
+    ////////////////// PROPERTY SETTERS /////////////////
+    /// ### Set `Node.wallet` property with `Wallet` instance.
     fn set_wallet(&mut self, wallet: Wallet) -> Result<()> {
         self.wallet = Some(wallet);
 
         Ok(())
     }
-    /// ## Set the wallet for the node with a filepath.
-    pub fn set_wallet_from_filepath(&mut self, filepath: &String) -> Result<()> {
-        let wallet = Wallet::new_from_file(filepath);
-
-        Ok(self.set_wallet(wallet)?)
-    }
-    /// ## INCOMPLETE Initialize the blockchain pulled from the peer to peer network.
-    ///
-    /// 1. Connect to p2p network
-    /// 1. Check with other nodes
-    /// 1. Fetch the hash of the blockchain
-    /// 1. Update blockchain if hash mismatch (get missing blocks)
-    /// Another setter for blockchain would be `set_blockchain_sync`
-    pub fn set_blockchain_init(&mut self) -> Result<()> {
-        todo!();
-        // self.blockchain = Some(Blockchain::new());
-
-        // Ok(())
-    }
-    /// ## INCOMPLETE Initialize the transaction pool pulled from the peer to peer network.
-    ///
-    /// 1. Connect to p2p network
-    /// 1. Check with other nodes
-    /// 1. Fetch the hash of the txn pool
-    /// 1. Update txn pool if hash mismatch (get missing txns)
-    /// Another setter for blockchain would be `set_txn_pool_sync`
-    pub fn set_txn_pool_init(&mut self) -> Result<()> {
-        todo!();
-        // self.blockchain = Some(Blockchain::new());
-
-        // Ok(())
-    }
-    /// ## Set p2p module
-    pub fn set_p2p(&mut self, host: IpAddr, port: u16) -> Result<()> {
-        self.p2p = Some(P2P::new(host, port));
+    /// ### Set `Node.blockchain` property with `P2P` instance.
+    #[allow(dead_code)]
+    fn set_blockchain(&mut self, blockchain: Blockchain) -> Result<()> {
+        self.blockchain = Some(blockchain);
 
         Ok(())
     }
-    ////////////////////////////// SETTERS //////////////////////////////
-    /////////////////////////////////////////////////////////////////////
+    /// ### Set `Node.txn_pool` property with `TxnPool` instance.
+    #[allow(dead_code)]
+    fn set_txn_pool(&mut self, txn_pool: TxnPool) -> Result<()> {
+        self.txn_pool = Some(txn_pool);
+
+        Ok(())
+    }
+    /// ### Set `Node.p2p` property with `P2P` instance.
+    fn set_p2p(&mut self, p2p: P2P) -> Result<()> {
+        self.p2p = Some(p2p);
+
+        Ok(())
+    }
+    ////////////////// PROPERTY SETTERS /////////////////
+    /////////////////////////////////////////////////////
 }
