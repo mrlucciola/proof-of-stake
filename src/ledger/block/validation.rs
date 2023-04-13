ce9bfe44664b0fb2471dbb0c953a99f52efe71b5
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-04-13 02:10:30 -0400

    chore: #94 fix block.signature inconsistencies

--- src/ledger/block/validation.rs
+++ src/ledger/block/validation.rs
@@ -5,7 +5,7 @@ use crate::ledger::general::PbKey;
 impl Block {
     /// ### Check if signature is valid.
     ///
-    /// 1. Assert there is a signature
+    /// 1. Assert there is a signature (not applicable currently - program panics)
     /// 1. Assert signature is valid
     ///
     /// Return is a Result of an Option to handle non-existant signatures
@@ -13,18 +13,13 @@ impl Block {
     /// - `None` indicates there is no signature
     /// - `Error` is for error handling
     pub fn is_signature_valid(&self, signer_pbkey: &PbKey) -> std::result::Result<(), BlockError> {
-        // 1) check if signature exists
-        if let None = &self.signature {
-            return Err(BlockError::EmptySignature.into());
-        }
-
         // create message for verification
         let msg: BlockDigest = self.calc_id().into();
         let mut presigned_msg = BLOCK_SIGNATURE_CTX.to_vec();
         presigned_msg.append(&mut msg.to_vec());
 
         // get the current signature
-        let block_signature = self.signature.clone().unwrap();
+        let block_signature = self.signature();
         let sig_test = ed25519::Signature::from_bytes(&block_signature.0).unwrap();
         let signer_conv: ed25519_dalek::PublicKey = signer_pbkey.into();
 
@@ -37,17 +32,11 @@ impl Block {
     /// ### Check if block is valid.
     ///
     /// Valid criteria:
-    ///   - all struct properties are not `None`
+    ///   - all struct properties are not `None` - (not applicable - default is panic)
     ///   - hash is valid
     ///   - signature is valid
     pub fn is_valid(&self, signer_pbkey: &PbKey) -> std::result::Result<(), BlockError> {
         // validate fields
-        if let None = self.signature {
-            return Err(BlockError::EmptySignature);
-        }
-        if let None = self.id {
-            return Err(BlockError::EmptyId);
-        };
         // validate hash
         if self.calc_id() != self.id() {
             return Err(BlockError::IncorrectId);
