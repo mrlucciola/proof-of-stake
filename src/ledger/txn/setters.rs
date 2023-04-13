6230897f98668ebe8e208e27ee55433a9b365b8b
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-04-13 02:16:20 -0400

    chore: #94 make txn.set_id private

--- src/ledger/txn/setters.rs
+++ src/ledger/txn/setters.rs
@@ -1,12 +1,3 @@
-use super::{Txn, TxnId};
+use super::Txn;
 
-impl Txn {
-    /// ## Get identifier (hash) for txn and set on txn object and store the output on the Txn object
-    /// Returns id.
-    pub fn set_id(&mut self) -> TxnId {
-        let id = self.calc_id();
-        self.id = Some(id);
-
-        id
-    }
-}
+impl Txn {}
