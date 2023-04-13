1ce0d58ddf6238e1396870a6084a65288775d1ab
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-04-13 02:16:20 -0400

    chore: #94 make txn.set_id private

--- src/ledger/txn/mod.rs
+++ src/ledger/txn/mod.rs
@@ -97,6 +97,14 @@ impl Txn {
 
     /////////////////////////////////////////////////
     //////////////// PRIVATE SETTERS ////////////////
+    /// ## Get identifier (hash) for txn and set on txn object and store the output on the Txn object
+    /// Returns id.
+    fn set_id(&mut self) -> TxnId {
+        let id = self.calc_id();
+        self.id = Some(id);
+
+        id
+    }
     /// ## Set the signature for the transaction.
     fn set_signature(&mut self, signature: TxnSignature) {
         self.signature = Some(signature);
