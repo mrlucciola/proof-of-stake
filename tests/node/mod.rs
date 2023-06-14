b692ea6d768fcb103d21afab08abb5a6bf762d67
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-06-13 23:18:57 -0400

    feat: #107 p2p- add peerid field and conversion

--- tests/node/mod.rs
+++ tests/node/mod.rs
@@ -5,26 +5,45 @@ use posbc::node::{p2p::P2P, types::Result, Node};
 // test
 use crate::common::fxns::init_blockchain_and_accounts;
 
+/// Test if the port is available using std lib.
+/// Attempt to open port at `localhost:port`.
+fn port_is_available(port: u16) -> bool {
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], port));
+
+    match std::net::TcpListener::bind(addr) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
 /// Test if the node is correctly using its environment to:
-/// 1. Check if node1 is running;
+/// 1. Check if node1 is running (on port `8898`);
 /// 1. Instantiate new node (node2, load wallet);
 /// 1. Connect to the peer to peer network (node1);
 /// 1. Check if node1's `peerId` exists;
 #[test]
 fn init_node_pass() -> Result<()> {
+    // 1. Test if node1 is running (port should be unavailable)
+    assert!(
+        port_is_available(8898) == false,
+        "node1 at Port 8898 is not running"
+    );
+
     let (users, _blockchain) = init_blockchain_and_accounts();
     let main = users.main;
+    let node1_pbkey = main.wallet.pbkey();
+
+    // 2. init node
     let host = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
-    let port = 8898;
-    let new_p2p = P2P::new(host, port);
+    let port = 8899;
+    println!("HERE0");
+    let p2p2 = P2P::new(host, port, &main.wallet.pbkey());
+    let mut node2 = Node::new(p2p2, main.wallet);
 
-    Node::new(new_p2p, main.wallet);
+    // 3. connect to node1
+    // node2.p2p_mut().unwrap().discover_peer(&node1_pbkey);
+    // node2.join()
 
-    // init node
-    // let node = Node::new();
-    // init wallet
-    // node.set_wallet(&users.main.filepath)?;
-    // node.set_wallet_from_filepath(&users.main.filepath)?;
     // assert_eq!(
     //     &node.wallet()?.pbkey(),
     //     &users.main.wallet.pbkey(),
