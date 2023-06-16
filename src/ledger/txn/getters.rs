16d185695838667311b5b656471e04f01a7a91f1
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-06-15 21:34:47 -0400

    feat: #109 add txn_header struct

--- src/ledger/txn/getters.rs
+++ src/ledger/txn/getters.rs
@@ -1,5 +1,5 @@
 // local
-use super::{Txn, TxnId, TxnSignature};
+use super::{Txn, TxnId, TxnSignature, TxnType};
 use crate::ledger::{general::PbKey, txn_pool::TxnMapKey};
 
 impl Txn {
@@ -16,12 +16,26 @@ impl Txn {
     pub fn signature(&self) -> &TxnSignature {
         self.signature.as_ref().unwrap()
     }
-    /// ### Getter for `Txn.pbkey_send` property
-    pub fn pbkey_send(&self) -> &PbKey {
-        &self.pbkey_send
+
+    // Getters for header properties
+    /// ### Get `Txn.amt` property
+    pub fn amt(&self) -> &u128 {
+        self.header.amt()
     }
-    /// ### Getter for `Txn.pbkey_recv` property
+    /// ### Get `Txn.pbkey_send` property
+    pub fn pbkey_send(&self) -> &PbKey {
+        self.header.pbkey_send()
+    }
+    /// ### Get `Txn.pbkey_recv` property
     pub fn pbkey_recv(&self) -> &PbKey {
-        &self.pbkey_recv
+        self.header.pbkey_recv()
+    }
+    /// ### Get `Txn.system_time` property
+    pub fn system_time(&self) -> &u64 {
+        self.header.system_time()
+    }
+    /// ### Get `Txn.txn_type` property
+    pub fn txn_type(&self) -> &TxnType {
+        self.header.txn_type()
     }
 }
